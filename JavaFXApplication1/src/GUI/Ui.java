b189e7d76ce930301c4b51a1561cf0e569d57cc6
CYRIL VELLA <cyril.vella@yahoo.com>
2018-01-30 16:16:17 +0100

    ajout plateau

--- JavaFXApplication1/src/GUI/Ui.java
+++ JavaFXApplication1/src/GUI/Ui.java
@@ -7,20 +7,22 @@ package GUI;
 
 import Jeu.Jeu;
 import Joueur.Joueur;
-import javafx.scene.image.Image;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.awt.Color;
 import java.util.ArrayList;
 import javafx.application.Application;
 import javafx.event.EventHandler;
+import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.scene.shape.*;
 import javafx.stage.Stage;
 import javafx.scene.image.ImageView;
+
+
 
 
 /**
@@ -33,7 +35,7 @@ public class Ui extends Application{
       public Joueur player1 = new Joueur("Joueur un", 0, "Bleu", 1);
       public Joueur player2 = new Joueur("Joueur deux", 0, "Rouge", 2);
       public GridPane root = new GridPane();
-        public Text text = new Text();
+      public Text text = new Text();
 
     @Override
     public void start(Stage primaryStage) {
@@ -57,31 +59,24 @@ public class Ui extends Application{
         
         VBox root = new VBox(2);
         root.getChildren().addAll(jouer, regle);
-        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.setScene(new Scene(root, 1000, 1000));
         primaryStage.show();
     }
   
-
-    
     public void jouer(Stage primaryStage)
     {
-        root.getChildren().removeAll();
-
-        Image img = new Image("accueil-ynov.png");
-
-        ImageView imgView = new ImageView(img);
-        ImageView imgView2 = new ImageView(img);
-        ImageView imgView3 = new ImageView(img);
-        ImageView imgView4 = new ImageView(img);
-
+        Group root = new Group();
+        Scene scene = new Scene(root, 1000, 1000);
+        Group g = new Group();
         
-        
-        redimensionnement(imgView);
-        redimensionnement(imgView2);
-        redimensionnement(imgView3);
-        redimensionnement(imgView4);
-        VBox root = new VBox(imgView, imgView2, imgView3, imgView4);
-        primaryStage.setScene(new Scene(root, 600, 600));
+        Rectangle rect = new Rectangle(290,140,200,200);
+        Rectangle rect2 = new Rectangle(290,350,200,200);
+        Rectangle rect3 = new Rectangle(500,140,200,200);
+        Rectangle rect4 = new Rectangle(500,350,200,200);
+
+        g.getChildren().addAll(rect, rect2, rect3, rect4);
+        root.getChildren().add(g);
+        primaryStage.setScene(scene);
         primaryStage.show();
     
         ArrayList Map = new ArrayList();
@@ -91,12 +86,6 @@ public class Ui extends Application{
         afficher(Map);
     }
     
-    public void redimensionnement(ImageView imgView)
-    {
-       imgView.setPreserveRatio(true);   
-       imgView.setFitHeight(100); 
-       imgView.setFitWidth(100);  
-    }
     
     public void afficher(ArrayList mapconsole)
     {
@@ -104,7 +93,6 @@ public class Ui extends Application{
         System.out.println(nbpannel);
     }
 
-    
     public void readregle(){
         String regles = "Voici les règles lorem ipsum latarae mes couilles et tout";
         text.setText(regles);
